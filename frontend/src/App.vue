588862b523dc50809d5225c50619ffbd6c672f85
Uadson Feitosa <uadsoncastro@gmail.com>
2022-07-01 15:08:02 -0300

    defining navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,47 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer />
 </template>
 
 <script>
-import Footer from './components/Footer.vue'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
 
   components: {
-    Footer,
     Navbar,
+    Footer,
+  },
+
+  data() {
+    return {
+      logo_src: '/img/logo.png',
+      alt: 'Make Your Burger',
+    }
   }
 }
 </script>
 
 <style>
+ * {
+  font-family: sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+ }
 
+ .main-container {
+  margin: 50px;
+  min-height: 250px;
+ }
+
+ h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+ }
 </style>
