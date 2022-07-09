1393032c8a4ac1cbf206a51f5346fc3a2fd32cdc
Uadson Feitosa <uadsoncastro@gmail.com>
2022-07-09 11:41:20 -0300

    deleted files

--- backend/api/routers/routers.py
+++
@@ -1,6 +0,0 @@
-from api.urls.urls import breads_url
-
-
-router = (
-    breads_url.router
-)
--- backend/api/urls/breads_url.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from api.views.breads_view import BreadsViewset
-
-
-router = SimpleRouter()
-router.register('breads', BreadsViewset)
