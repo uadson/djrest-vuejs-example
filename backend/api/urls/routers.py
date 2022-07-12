4f0aaf5623802f81dc922e34519267eacceb2220
Uadson Feitosa <uadsoncastro@gmail.com>
2022-07-12 17:45:42 -0300

    created route for order

--- backend/api/urls/routers.py
+++ backend/api/urls/routers.py
@@ -3,6 +3,7 @@ from rest_framework.routers import SimpleRouter
 # Views
 from api.views.breads_view import BreadsViewset
 from api.views.meats_view import MeatsViewset
+from api.views.orders_view import OrdersViewset
 
 
 # Routers
@@ -10,3 +11,4 @@ router = SimpleRouter()
 
 router.register('breads', BreadsViewset)
 router.register('meats', MeatsViewset)
+router.register('orders', OrdersViewset)
