b380a9684cfb6b4bc8ccab9f5e28d67719ed0e81
Uadson Feitosa <uadsonpy@gmail.com>
2022-07-15 17:30:27 -0300

    defining model order

--- backend/api/models/orders.py
+++ backend/api/models/orders.py
@@ -3,6 +3,9 @@ from django.db import models
 from api.models.base import Base
 from api.models.breads import Bread
 from api.models.meats import Meat
+from api.models.options import Option
+
+from datetime import datetime
 
 
 class Order(Base):
@@ -14,13 +17,20 @@ class Order(Base):
     customer = models.CharField('Customer', max_length=200)
     bread = models.ForeignKey(Bread, on_delete=models.CASCADE)
     meat = models.ForeignKey(Meat, on_delete=models.CASCADE)
+    options = models.ManyToManyField(Option)
     status = models.CharField(
-        'Status', choices=STATUS_CHOICES, default='Solicitado', max_length=11)
+        'Status', choices=STATUS_CHOICES, default='Solicitado',
+        max_length=11, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
         db_table = 'order'
+
+    def save(self, *args, **kwargs):
+        self.created = self.created.datetime.strftime('%d/%m/%Y, %H:%M h')
+        self.updated = self.updated.datetime.strftime('%d/%m/%Y, %H:%M h')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Order n.: {self.id}'
