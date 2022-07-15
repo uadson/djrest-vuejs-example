884b61485f13f26d7e7d84cf9906dd5824da0312
Uadson Feitosa <uadsonpy@gmail.com>
2022-07-15 17:21:21 -0300

    changed name axios file for api.js inside the folder services in src

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 
-const getAPI = axios.create({
+const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/burger/v1',
-    timeout: 1000
 })
 
-export { getAPI }
+export default api
